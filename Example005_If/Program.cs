bb205c016437804d11b558287906a26c7d31c70a
lyal1ch1 <106307360+lyal1ch1@users.noreply.github.com>
2022-11-28 22:29:48 +0300

    cycle if

---
+++ Example005_If/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите свое имя:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "оля" )
+{
+    Console.WriteLine("ООО, дарова Оля");
+}
+ else
+{
+    Console.Write("Привет, ");  
+    Console.WriteLine (username);
+}
